8114b07c1deab2085b71d545d9e4d7071e0900ba
Anna <ammstir@gmail.com>
2018-09-30 20:43:35 +0300

    ДЗ-8. Стикеры на уточках

--- test_login.py
+++ test_login.py
@@ -7,13 +7,28 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture
 def driver(request):
     wd = webdriver.Firefox(capabilities={"marionette": False})
+    # wd = webdriver.Chrome()
+    # wd = webdriver.Safari()
     request.addfinalizer(wd.quit)
     return wd
 
+#
+# def test_go_through_sidebar(driver):
+#     driver.get("http://localhost/litecart/admin/")
+#     driver.find_element_by_name("username").send_keys("admin")
+#     driver.find_element_by_name("password").send_keys("admin")
+#     driver.find_element_by_name("login").click()
+#     driver.implicitly_wait(20)
+#     menu = driver.find_element_by_css_selector("ul#box-apps-menu")
+#     link_in_menu = list(menu.find_elements_by_tag_name("a"))
+#     for link in link_in_menu:
+#         link.click()
 
-def test_example(driver):
-    driver.get("http://localhost/litecart/admin/")
-    driver.find_element_by_name("username").send_keys("admin")
-    driver.find_element_by_name("password").send_keys("admin")
-    driver.find_element_by_name("login").click()
-    WebDriverWait(driver, 10).until(EC.title_is("My Store"))
+
+def test_check_stickers(driver):
+    driver.get("http://localhost/litecart/en/")
+    driver.implicitly_wait(10)
+    ducks = driver.find_elements_by_css_selector("div.image-wrapper")
+    stickers = driver.find_elements_by_css_selector("div.sticker")
+    assert len(list(ducks)) == len(list(stickers))
+
