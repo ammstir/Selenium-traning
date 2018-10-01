09d00039dcf7a7711be4193b684322aee79909be
Anna <ammstir@gmail.com>
2018-10-01 22:40:02 +0300

    ДЗ-8. Утки на Стикерах

--- test/test_check_stikers.py
+++ test/test_check_stikers.py
@@ -2,6 +2,7 @@
 def test_check_stickers(app):
     wd = app.wd
     app.session.open_litecart()
-    ducks = wd.find_elements_by_css_selector("div.image-wrapper")
-    stickers = wd.find_elements_by_css_selector("div.sticker")
-    assert len(list(ducks)) == len(list(stickers))
+    ducks = wd.find_elements_by_css_selector("li.product.column.shadow.hover-light")
+    for duck in ducks:
+        stickers = duck.find_elements_by_css_selector("a.link[title$=Duck] div.sticker")
+        assert len(stickers) <= 1
