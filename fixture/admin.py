988856cce4fddeb9cd5f0a90e2ffb97455ae83dd
Anna <ammstir@gmail.com>
2018-10-01 23:24:50 +0300

    ДЗ-9. Обход стран

--- fixture/admin.py
+++ fixture/admin.py
@@ -5,9 +5,10 @@ class Admin:
     def __init__(self, app):
         self.app = app
 
-    def get_countries_list(self, table):
+    def get_countries_list(self):
+        wd = self.app.wd
         country_list = []
-        rows = table.find_elements_by_css_selector("tr.row")
+        rows = wd.find_elements_by_css_selector("table.dataTable tr.row")
         for row in rows:
-            country_list.append(row.find_element_by_tag_name("[href*=country_code]:not([title='Edit'])").text)
+            country_list.append(row.find_element_by_css_selector("a[href*=country_code]:not([title=Edit])").text)
         return country_list
