b425b818a50d160bfc07204a80c9135e52e061be
Anna <ammstir@gmail.com>
2018-10-02 18:56:04 +0300

    ДЗ-8. Обход стикеров на утках

--- test/test_check_stikers.py
+++ test/test_check_stikers.py
@@ -2,7 +2,7 @@
 def test_check_stickers(app):
     wd = app.wd
     app.session.open_litecart()
-    ducks = wd.find_elements_by_css_selector("li.product.column.shadow.hover-light")
+    ducks = wd.find_elements_by_css_selector("a.link[title$=Duck]")
     for duck in ducks:
-        stickers = duck.find_elements_by_css_selector("a.link[title$=Duck] div.sticker")
-        assert len(stickers) <= 1
+        stickers = duck.find_elements_by_css_selector("div.sticker")
+        assert len(stickers) == 1
