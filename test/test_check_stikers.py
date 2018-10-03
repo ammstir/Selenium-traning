49ad34569bd4fe0dfd3c9b91731c4a6a024485bd
Anna <ammstir@gmail.com>
2018-10-03 13:11:46 +0300

    ДЗ-8. Стикеры на утках с идеальными локаторами

--- test/test_check_stikers.py
+++ test/test_check_stikers.py
@@ -2,7 +2,7 @@
 def test_check_stickers(app):
     wd = app.wd
     app.session.open_litecart()
-    ducks = wd.find_elements_by_css_selector("li.product a.link[title$=Duck]")
+    ducks = wd.find_elements_by_css_selector("li.product")
     for duck in ducks:
         stickers = duck.find_elements_by_css_selector("div.sticker")
         assert len(stickers) == 1
