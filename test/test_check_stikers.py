341af453d8e289cf5fb6e38c932a62e8e8dc345e
Anna <ammstir@gmail.com>
2018-10-24 14:57:53 +0300

    логгер

--- test/test_check_stikers.py
+++ test/test_check_stikers.py
@@ -1,6 +1,19 @@
+from selenium.webdriver.support.events import EventFiringWebDriver, AbstractEventListener
+
+
+class MyListener(AbstractEventListener):
+    def before_find(self, by, value, driver):
+        print(by, value)
+
+    def after_find(self, by, value, driver):
+        print(by, value, "found")
+
+    def on_exception(self, exception, driver):
+        print(exception)
+
 
 def test_check_stickers(app):
-    wd = app.wd
+    wd = EventFiringWebDriver(app.wd, MyListener())
     app.session.open_litecart_homepage()
     ducks = wd.find_elements_by_css_selector("li.product")
     for duck in ducks:
